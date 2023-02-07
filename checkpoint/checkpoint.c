dd6411f1a190a9d039ba096230fc60d988863e56
eNiiju <noemaillet@03@gmail.com>
2023-02-07 10:20:29 +0100

    Set up main threads
    Co-authored-by: merc76 <merc76@users.noreply.github.com>

--- checkpoint/checkpoint.c
+++ checkpoint/checkpoint.c
@@ -1,16 +1,76 @@
 /**
  * Main file for the checkpoint's program.
- * @file main.c
+ * @file checkpoint.c
  * @author Noé Maillet & Mathéo Mercier
  * @date 2023-02-07
  *
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 #include "checkpoint.h"
+
+/* ------------------------------------------------------------------------- */
+/*                               Main function                               */
+/* ------------------------------------------------------------------------- */
 
 int main(int argc, char* argv[])
 {
-    printf("Hello World!\n");
-    
+    pthread_t th_display, th_button, th_rfid, th_send_data, th_usb_key;
+
+    // Create threads
+    pthread_create(&th_display, NULL, display_routine, NULL);
+    pthread_create(&th_button, NULL, button_routine, NULL);
+    pthread_create(&th_rfid, NULL, rfid_routine, NULL);
+    pthread_create(&th_send_data, NULL, send_data_routine, NULL);
+    pthread_create(&th_usb_key, NULL, usb_key_routine, NULL);
+
+    // Wait for threads to finish
+    pthread_join(th_display, NULL);
+    pthread_join(th_button, NULL);
+    pthread_join(th_rfid, NULL);
+    pthread_join(th_send_data, NULL);
+    pthread_join(th_usb_key, NULL);
+
     return 0;
+}
+
+/* ------------------------------------------------------------------------- */
+/*                                 Functions                                 */
+/* ------------------------------------------------------------------------- */
+
+void* display_routine(void* arg)
+{
+    printf("Display routine\n");
+
+    pthread_exit(NULL);
+}
+
+void* button_routine(void* arg)
+{
+    printf("Button routine\n");
+
+    pthread_exit(NULL);
+}
+
+void* rfid_routine(void* arg)
+{
+    printf("RFID routine\n");
+
+    pthread_exit(NULL);
+}
+
+void* send_data_routine(void* arg)
+{
+    printf("Send data routine\n");
+
+    pthread_exit(NULL);
+}
+
+void* usb_key_routine(void* arg)
+{
+    printf("USB key routine\n");
+
+    pthread_exit(NULL);
 }
--- checkpoint/checkpoint.h
+++ checkpoint/checkpoint.h
@@ -1,1 +1,52 @@
-#include <stdio.h>
+/**
+ * Header of the main file for the checkpoint's program.
+ * @file checkpoint.h
+ * @author Noé Maillet & Mathéo Mercier
+ * @date 2023-02-07
+ *
+*/
+
+/* ------------------------------------------------------------------------- */
+/*                          Constants & Definitions                          */
+/* ------------------------------------------------------------------------- */
+
+
+
+/* ------------------------------------------------------------------------- */
+/*                            Function prototypes                            */
+/* ------------------------------------------------------------------------- */
+
+/**
+ * Function for the display thread.
+ * It is responsible for displaying informations on the screen.
+ * @param arg Unused.
+*/
+void* display_routine(void* arg);
+
+/**
+ * Function for the button thread.
+ * It is responsible for handling the button interrupts.
+ * @param arg Unused.
+*/
+void* button_routine(void* arg);
+
+/**
+ * Function for the RFID thread.
+ * It is responsible for handling the RFID interrupts.
+ * @param arg Unused.
+*/
+void* rfid_routine(void* arg);
+
+/**
+ * Function for the send data thread.
+ * It is responsible for sending data to the Kinéis satellites.
+ * @param arg Unused.
+*/
+void* send_data_routine(void* arg);
+
+/**
+ * Function for the USB key thread.
+ * It is responsible for handling the USB key interactions.
+ * @param arg Unused.
+*/
+void* usb_key_routine(void* arg);
