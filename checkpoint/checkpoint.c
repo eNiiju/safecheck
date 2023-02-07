3635bb95decc30f838c5097883a9e60f46d2210e
Noé M <60700919+eNiiju@users.noreply.github.com>
2023-02-07 09:19:40 +0100

    Checkpoint skeleton

---
+++ checkpoint/checkpoint.c
@@ -0,0 +1,16 @@
+/**
+ * Main file for the checkpoint's program.
+ * @file main.c
+ * @author Noé Maillet & Mathéo Mercier
+ * @date 2023-02-07
+ *
+*/
+
+#include "checkpoint.h"
+
+int main(int argc, char* argv[])
+{
+    printf("Hello World!\n");
+    
+    return 0;
+}
