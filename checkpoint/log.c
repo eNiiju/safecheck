16d9f6d6122232a23b8836584719939bcab1f82c
Noe M <60700919+eNiiju@users.noreply.github.com>
2023-03-12 18:19:12 +0100

    Add log functions
    * Add functions to create a log
    * Add log functions

--- checkpoint/log.c
+++ checkpoint/log.c
@@ -8,8 +8,104 @@
 
 #include "log.h"
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 /* ------------------------------------------------------------------------- */
 /*                                 Functions                                 */
 /* ------------------------------------------------------------------------- */
 
+bool create_log(int id, char* description)
+{
+    log_t log;
+    time_t timestamp = time(NULL);
+    FILE* log_file = fopen(PATH_LOG, "a"); // Open in append mode
 
+    if (log_file == NULL) return false;
+
+    // Create log struct, copy description until MAX_LENGTH_DESCRIPTION
+    log.timestamp = timestamp;
+    log.id = id;
+    strncpy(log.description, description, MAX_LENGTH_DESCRIPTION);
+
+    // Write log data to file
+    fprintf(log_file, "%ld %d \"%s\"\n", log.timestamp, log.id, log.description);
+
+    fclose(log_file);
+    return true;
+}
+
+bool create_emergency_log(void)
+{
+    return create_log(EMERGENCY_ID, EMERGENCY_DESCRIPTION);
+}
+
+bool create_emergency_solved_log(void)
+{
+    return create_log(EMERGENCY_SOLVED_ID, EMERGENCY_SOLVED_DESCRIPTION);
+}
+
+bool read_log(log_t* log, int index)
+{
+    FILE* log_file = fopen(PATH_LOG, "r");
+    char line[MAX_LENGTH_LINE];
+
+    if (log_file == NULL) return false;
+
+    for (int i = 0; fgets(line, sizeof(line), log_file) != NULL; i++) {
+        if (i != index) continue;
+
+        // Split line into timestamp, id and description
+        char* timestamp = strtok(line, " ");
+        char* id = strtok(NULL, " \"");
+        char* description = strtok(NULL, "\"");
+
+        // Copy data to log struct
+        log->timestamp = atol(timestamp);
+        log->id = atoi(id);
+        strcpy(log->description, description);
+    }
+
+    fclose(log_file);
+    return true;
+}
+
+int count_logs(void)
+{
+    FILE* log_file = fopen(PATH_LOG, "r");
+    char line[MAX_LENGTH_LINE];
+    int count = 0;
+
+    if (log_file == NULL) return 0;
+
+    while (fgets(line, sizeof(line), log_file) != NULL)
+        count++;
+
+    fclose(log_file);
+    return count;
+}
+
+bool is_emergency_active(void)
+{
+    FILE* log_file = fopen(PATH_LOG, "r");
+    char line[MAX_LENGTH_LINE];
+    bool emergency_active = false;
+
+    if (log_file == NULL) return false;
+
+    while (fgets(line, sizeof(line), log_file) != NULL) {
+        // Split line to get the id
+        strtok(line, " ");
+        char* id = strtok(NULL, " \"");
+
+        // Update emergency_active if id is EMERGENCY_ID or EMERGENCY_SOLVED_ID
+        if (atoi(id) == EMERGENCY_ID)
+            emergency_active = true;
+        else if (atoi(id) == EMERGENCY_SOLVED_ID)
+            emergency_active = false;
+    }
+
+    fclose(log_file);
+    return emergency_active;
+}
--- checkpoint/log.h
+++ checkpoint/log.h
@@ -10,6 +10,7 @@
 #define LOG_H
 
 #include <time.h>
+#include <stdbool.h>
 
 /* ------------------------------------------------------------------------- */
 /*                          Constants & Definitions                          */
@@ -17,8 +18,11 @@
 
 #define PATH_LOG "./resources/safecheck.log"
 #define MAX_LENGTH_DESCRIPTION 256
-#define ID_EMERGENCY -1
-#define ID_EMERGENCY_SOLVED -2
+#define MAX_LENGTH_LINE 10 /* timestamp */ + 11 /* id */ + 2 /* quotes */ + MAX_LENGTH_DESCRIPTION + 3 /* spaces & newline */
+#define EMERGENCY_ID -1
+#define EMERGENCY_SOLVED_ID -2
+#define EMERGENCY_DESCRIPTION "Emergency button pressed"
+#define EMERGENCY_SOLVED_DESCRIPTION "Emergency solved"
 
 /* ------------------------------------------------------------------------- */
 /*                             Type definitions                              */
@@ -29,10 +33,54 @@
 */
 struct log {
     time_t timestamp;
-    int id; // ID_EMERGENCY for emergency, ID_EMERGENCY_SOLVED for emergency solved
+    int id; // Runner ID or ID_EMERGENCY for emergency, ID_EMERGENCY_SOLVED for emergency solved
     char description[MAX_LENGTH_DESCRIPTION]; // The participant's name, or emergency description
 };
 
 typedef struct log log_t;
+
+/* ------------------------------------------------------------------------- */
+/*                            Function prototypes                            */
+/* ------------------------------------------------------------------------- */
+
+/**
+ * Creates a log entry in the log file.
+ * @param id The ID of the runner, or a special ID.
+ * @param description The description of the log entry. (e.g. the runner's name)
+ * @return True if the log entry was created successfully, false otherwise.
+*/
+bool create_log(int id, char* description);
+
+/**
+ * Creates an emergency log entry in the log file.
+ * @return True if the log entry was created successfully, false otherwise.
+*/
+bool create_emergency_log(void);
+
+/**
+ * Creates an emergency solved log entry in the log file.
+ * @return True if the log entry was created successfully, false otherwise.
+*/
+bool create_emergency_solved_log(void);
+
+/**
+ * Reads a log entry from the log file.
+ * @param log Pointer to the log struct to fill.
+ * @param index The index of the log entry to read. (0 = first line)
+ * @return True if the log entry was read successfully, false otherwise.
+*/
+bool read_log(log_t* log, int index);
+
+/**
+ * Counts the number of log entries in the log file.
+ * @return The number of log entries.
+*/
+int count_logs(void);
+
+/**
+ * Returns the last emergency state.
+ * @return True if an emergency is active, false otherwise.
+*/
+bool is_emergency_active(void);
 
 #endif
