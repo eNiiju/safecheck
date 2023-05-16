850ee5addedfdd6f4d9cd275555c7fcca489cf99
Noé M <60700919+eNiiju@users.noreply.github.com>
2023-05-16 22:06:36 +0200

    Test display update

--- checkpoint/test/test_display.c
+++ checkpoint/test/test_display.c
@@ -6,33 +6,49 @@
 
 #include "../src/display.h"
 
+#include <stdbool.h>
+
+ssd1306_i2c_t* p_display;
+ssd1306_framebuffer_t* p_framebuffer;
+
 int main(void)
 {
-    ssd1306_framebuffer_box_t bbox;
-    ssd1306_i2c_t* p_display;
-    ssd1306_framebuffer_t* p_framebuffer;
-    display_tab_t tab = { "jean", "jacques", "goldman", "anyway", "jsplus", "aled", "j'ai", "pas", "de", "phrase", "drole", "a", "ecrir" };
+    struct display_message message = {
+        .length = 13,
+        .lines = { "jean", "jacques", "goldman", "anyway", "jsplus", "aled", "j'ai", "pas", "de", "phrase", "drole", "a", "ecrir" }
+    };
 
-    //************************init**************************//
-    //I previously made an init func but the library doesn't like me using 
-    //passing references by address so I just put the code here
-    p_display = ssd1306_i2c_open(BUS_ADDRESS, DEVICE_ADDRESS, DISPLAY_WIDTH, DISPLAY_HEIGHT, NULL);
+    init_display();
+
+    display_string(p_display, p_framebuffer, "Hello World!\nAHAHaAHHAAAahahaaahahanoe\nmat BG", 4, 16, 4);
+    sleep(5);
+
+    display_message_array(p_display, p_framebuffer, message, message.length);
+    sleep(5);
+
+    display_ok(p_display, p_framebuffer, false);
+    sleep(5);
+
+    display_error(p_display, p_framebuffer, false);
+    sleep(5);
+
+    display_clear(p_display, p_framebuffer);
+
+    return 0;
+}
+
+void init_display()
+{
+    p_display = ssd1306_i2c_open(DISPLAY_I2C_BUS_ADDRESS, DISPLAY_I2C_DEVICE_ADDRESS, DISPLAY_WIDTH, DISPLAY_HEIGHT, NULL);
     if (!p_display) {
         printf("Error: Could not open display\n");
-        return -1;
+        return false;
     }
     if (ssd1306_i2c_display_initialize(p_display) < 0) {
         printf("Error: Could not initialize display\n");
         ssd1306_i2c_close(p_display);
-        return -1;
+        return false;
     }
     sleep(3);
     p_framebuffer = ssd1306_framebuffer_create(p_display->width, p_display->height, p_display->err);
-    //**********************end init**************************//
-
-    write_string_to_display(p_display, p_framebuffer, "Hello World!\npd AHAHaAHHAAAahahaaahahanoe\nmat BG", 4, 16, 4);
-    write_tab_to_display_scroll(p_display, p_framebuffer, tab, 14);
-    write_valid_to_display(p_display, p_framebuffer);
-    sleep(5);
-    write_error_to_display(p_display, p_framebuffer);
 }
